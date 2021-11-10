2eeebf9337e5aabd0988425ea6197c551be9e119
konradkrawczykpl <konradkrawczyk96@gmail.com>
2021-11-10 18:25:08 +0100

    Collars

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String am = "collar design";
+        String ku = "total core";
+        System.out.println(am+ku);
+
+    }
+}
